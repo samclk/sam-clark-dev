a702b4ce3c4006398ca2a3f890887568fb8dc40e
Sam Clark <samclark@Sams-MacBook-Pro.local>
2022-07-11 14:04:03 +0100

    fix: move values to env vars

--- pages/api/mail.tsx
+++ pages/api/mail.tsx
@@ -2,10 +2,14 @@ import { NextApiHandler } from 'next'
 import sendgrid from '@sendgrid/mail'
 
 const handler: NextApiHandler = async (req, res) => {
-  sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string)
-  if (!process.env.SENDGRID_API_KEY) {
+  const { EMAIL_TO, EMAIL_FROM, SENDGRID_API_KEY } = process.env
+
+  if (!EMAIL_TO || !EMAIL_FROM || !SENDGRID_API_KEY) {
     res.status(500).json({ reason: `Missing env variable` })
+    return
   }
+
+  sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string)
 
   const body = JSON.parse(req.body)
 
@@ -16,8 +20,8 @@ const handler: NextApiHandler = async (req, res) => {
     `
 
   const data = {
-    to: 'samclark.dev@gmail.com',
-    from: 'samclark.dev@gmail.com',
+    to: EMAIL_TO,
+    from: EMAIL_FROM,
     subject: '[Sam Clark Dev] Contact Form - New Message',
     text: message,
     html: message.replace(/\r\n/g, '<br>'),
@@ -27,7 +31,7 @@ const handler: NextApiHandler = async (req, res) => {
     await sendgrid.send(data)
     res.status(200).json({ name: 'Email sent' })
   } catch (error) {
-    res.status(500).json({ reason: 'Could not send mail' })
+    res.status(500).json({ error })
   }
 }
 
