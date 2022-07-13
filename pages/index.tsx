a080c98e9314fde1d8a6614f9dfdd240c5d0d458
Sam Clark <samclark@Sams-MacBook-Pro.local>
2022-07-13 22:52:28 +0100

    feat: add google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import ContactForm from '../components/ContactForm'
 import ImageSlider from '../components/ImageSlider'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 const Home: NextPage = () => {
   const [imageLoaded, setImageLoaded] = React.useState(false)
@@ -103,6 +104,21 @@ const Home: NextPage = () => {
         </motion.div>
       </motion.div>
       <ContactForm isVisible={formIsVisible} closeForm={closeForm} />
+
+      {/* Add Google Tag */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});
+        `}
+      </Script>
     </Layout>
   )
 }
