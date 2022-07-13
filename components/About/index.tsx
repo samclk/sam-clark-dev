12cda61adc8788e808a86160c30bc6208a1a148f
Sam Clark <samclark@Sams-MacBook-Pro.local>
2022-07-13 13:23:55 +0100

    fix: update font and add icon

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 interface AboutProps {
   closeAbout: () => void
@@ -42,19 +43,19 @@ const About: React.FC<AboutProps> = ({ closeAbout }) => {
         <h2 className="mb-8 text-2xl">
           Hi, I’m <span className="text-pink-600">Sam</span>
         </h2>
-        <p className="mb-6">
+        <p className="mb-6 text-lg text-gray-800">
           A freelance web developer based in King’s Lynn. I have 10 years
           experience working in creative design agencies on a variety of
           projects from start ups to established applications.
         </p>
-        <p className="mb-6">
+        <p className="mb-6 text-gray-600">
           I specialise in full stack applications using a variety of
           technologies but primarily focused on Typescript, Next.js, React, Node
           and Cypress. I am always looking to expand my toolkit but also see
           value in spending time to learn and incorporate a piece of tech into
           my workflow before always jumping to the ‘next big thing’.
         </p>
-        <p className="mb-6">
+        <p className="mb-6 text-gray-600">
           Having graduated in animation technology at university I was able to
           take what was a passionate hobby in development and turn it into my
           career. This has lead me to not only understand the requirements to
@@ -62,9 +63,10 @@ const About: React.FC<AboutProps> = ({ closeAbout }) => {
           that I can be proud of.
         </p>
         <button
-          className="block cursor-pointer text-xs uppercase text-pink-600"
+          className="flex cursor-pointer items-center text-xs uppercase text-pink-600"
           onClick={closeAbout}
         >
+          <AiOutlineArrowLeft />
           Back
         </button>
       </div>
